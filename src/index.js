8940709d5f8913b654d5c51e6aff17bf08bd0ef3
zero86 <z1004man@naver.com>
2024-04-04 21:34:41 +0900

    cors 허용

--- src/index.js
+++ src/index.js
@@ -1,26 +1,31 @@
 const express = require('express');
 const path = require("path");
+const cors = require('cors');
 
-const index = express();
-index.use(express.json());
-index.use(express.urlencoded({extended: true}));
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use('/static', express.static(path.join(process.cwd(), 'public')));
 
-index.use('/static', express.static(path.join(process.cwd(), 'public')));
+app.use(cors({
+    origin: ['http://localhost:5173'], // 모든 출처 허용
+    credentials: true, // 인증 정보 허용
+}));
 
 
-index.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.json('hello express')
 });
 
-index.get('/index', (req, res) => {
+app.get('/index', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-index.post('/login', (req, res) => {
+app.post('/login', (req, res) => {
     res.json('login success');
 });
 
 const PORT = 3000;
-index.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`listen to server ${PORT}`)
 })
